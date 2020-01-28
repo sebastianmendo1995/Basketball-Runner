da43a1f07515175a8b72622d50b553a34f4b12fc
Sebastian Mendo Lopez <mendo@Sebastians-MacBook-Pro.local>
2020-01-27 16:41:39 -0800

    Webpack setup

---
+++ src/baskeball_runner.js
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  const gameCanvas = document.getElementById('game-canvas');
+  const canvasContext = gameCanvas.getContext('2d');
+
+  const backgroundCanvas = document.getElementById('background-canvas');
+  const backgroundCanvasContext = backgroundCanvas.getContext('2d');
+
+  const foregroundCanvas = document.getElementById('foreground-canvas');
+  const foregroundCanvasContext = foregroundCanvas.getContext('2d');
+
+//   const game = new Game(
+//     canvasContext,
+//     gameCanvas,
+//     backgroundCanvasContext,
+//     foregroundCanvasContext
+//   );
+
+//   game.openMenu();
+
+});
